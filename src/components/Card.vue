8d3895c142a2e8a3fdc2cf952f937296cc17c0f8
Mohammed Alnahas <malnahas@stu.southtexascollege.edu>
2026-09-10 22:17:06 -0500

    Added Card, changed

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="card">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+.card {
+  width: min(600 px, 100%);
+  background: #e7e4e4;
+  padding: 18px;
+  border-radius: 16px;
+  box-shadow: 0 10px 24px rgba(94, 94, 94, 0.1);
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="top"> 
-    <h1>Balance Calculation</h1>
+    <h1>Balance Calculation in Vue</h1>
     <p>Split expenses and see who owns what.</p>    
   </header>
 </template>
